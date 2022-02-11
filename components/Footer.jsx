ff5156a527cd9840bf57f01b46d82cddc94b25c9
Caio Santos da Silva <vorreicaio@hotmail.com>
2022-02-10 20:30:24 -0600

    footer rel fix

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer(params) {
             <footer className="p-4 bg-black text-white sm:p-6 dark:bg-gray-800">
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
-                        <a href="https://flowbite.com" target="_blank" className="flex items-center">
+                        <a href="https://flowbite.com" target="_blank" rel="noopener noreferrer" className="flex items-center">
 
                             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
                         </a>
@@ -56,7 +56,7 @@ export default function Footer(params) {
 
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com"
-                        target="_blank" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+                        target="_blank" rel="noopener noreferrer" className="hover:underline">Flowbite™</a>. All Rights Reserved.
                     </span>
                     <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
                         <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
